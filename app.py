a7390e4af2eb7e1cacb21ace164c2c8da03ab3ce
Matheus Gomes <matheusbgomes4@gmail.com>
2025-07-22 21:10:14 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from dash import Dash
+import dash_bootstrap_components as dbc
+
+
+app = Dash(__name__, 
+           external_stylesheets=['assets/main.css', dbc.themes.FLATLY],
+           suppress_callback_exceptions=True)
+
+        
+            
+
+
